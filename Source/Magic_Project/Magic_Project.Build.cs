7ddf724bd4e85ffe6531b567bf9a09e1e3301a6f
kaigara087 <24ci0204@jec.ac.jp>
2025-04-16 19:52:58 +0900

    Magic_Project.Build.csの追加
    自分のところで起こったコンフリクト対応

--- Source/Magic_Project/Magic_Project.Build.cs
+++ Source/Magic_Project/Magic_Project.Build.cs
@@ -8,7 +8,7 @@ public class Magic_Project : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG", "EnhancedInput" ,"Niagara"});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
