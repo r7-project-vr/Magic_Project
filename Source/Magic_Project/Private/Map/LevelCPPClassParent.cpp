f7339d50c8016fc1689b92a0e764f2fa8988161e
satou <24CI0412@jec.ac.jp>
2025-04-16 19:37:26 +0900

    satou

---
+++ Source/Magic_Project/Private/Map/LevelCPPClassParent.cpp
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Map/LevelCPPClassParent.h"
+#include "Components/StaticMeshComponent.h"
+
+AMyActor::AMyActor()
+{
+    PrimaryActorTick.bCanEverTick = true;
+
+    // コンポーネントの作成
+    MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComponent"));
+    RootComponent = MeshComponent;
+}
+
+void AMyActor::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AMyActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
