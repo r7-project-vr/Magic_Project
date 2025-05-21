b437b30b914084b5ae0f9f0e0b67fd948c8442ba
kaigara087 <24ci0204@jec.ac.jp>
2025-05-21 19:38:35 +0900

    魔法陣classのファイルを作成

---
+++ Source/Magic_Project/Private/Magic/Onishi_MagicCircleParent.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Magic/Onishi_MagicCircleParent.h"
+
+// Sets default values
+AOnishi_MagicCircleParent::AOnishi_MagicCircleParent()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AOnishi_MagicCircleParent::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AOnishi_MagicCircleParent::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
