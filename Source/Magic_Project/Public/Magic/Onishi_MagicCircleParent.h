c934a4013f32ca3a9c4ea4a23176cf706a735e66
kaigara087 <24ci0204@jec.ac.jp>
2025-05-23 11:18:13 +0900

    魔法陣の親クラス作成
    ・魔法陣のエフェクトが1回だけでる
    　・無限ループにする必要がある
    ・魔法陣に入ったときHIT、出たときENDと出る
    ・上に合わせて、魔法を打てるフラグが変化する

--- Source/Magic_Project/Public/Magic/Onishi_MagicCircleParent.h
+++ Source/Magic_Project/Public/Magic/Onishi_MagicCircleParent.h
@@ -4,7 +4,10 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "NiagaraSystem.h"
+#include "NiagaraComponent.h"
 #include "Onishi_MagicCircleParent.generated.h"
+
 
 UCLASS()
 class MAGIC_PROJECT_API AOnishi_MagicCircleParent : public AActor
@@ -23,4 +26,29 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	//スフィアコリジョン
+	UPROPERTY(VisibleAnywhere)
+	class USphereComponent* SphereComponent;
+
+	//オーバーラップした際の処理を書く関数
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult& SweepResult);
+
+	//オーバーラップから抜けた際の処理を書く関数
+	UFUNCTION()
+	void OnOverlapEnd(UPrimitiveComponent* OverlappedComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex);
+
+	//魔法陣としてのエフェクトのPath
+	UPROPERTY(BluePrintReadWrite, Category = "Effects")
+	UNiagaraSystem* Ef_MagicCircle;
+
+	bool Shootable = false;
 };
