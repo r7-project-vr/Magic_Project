e348dde09ca5d7a292851ab713860fe34390a8a7
satou <24CI0412@jec.ac.jp>
2025-08-08 19:43:30 +0900

    ASerialの追加

--- Source/Magic_Project/Magic_Project.Build.cs
+++ Source/Magic_Project/Magic_Project.Build.cs
@@ -8,7 +8,7 @@ public class Magic_Project : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG", "Niagara", "HeadMountedDisplay" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG", "Niagara", "HeadMountedDisplay", "ASerialCom" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
