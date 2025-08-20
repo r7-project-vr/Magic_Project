8fc08641f1b9ba20036a6a6cf21499485110f1a1
satou-keigo <24CI0412@jec.ac.jp>
2025-08-20 23:57:00 +0900

    a

--- Source/Magic_Project/Private/Kanda/VRActor_ver1.cpp
+++ Source/Magic_Project/Private/Kanda/VRActor_ver1.cpp
@@ -17,15 +17,16 @@
 #include "Engine/Engine.h"
 #include "IXRTrackingSystem.h"
 #include "HeadMountedDisplay.h"
+#include "Components/SplineComponent.h"
 #include "Kismet/KismetMathLibrary.h"
 
 
 // Sets default values
-AVRActor_ver1::AVRActor_ver1():
+AVRActor_ver1::AVRActor_ver1() :
 	magicData(nullptr),
 	circle(nullptr)
 {
- 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
 	// StaticMeshComponentを追加し、RootComponentに設定する
@@ -64,6 +65,9 @@ AVRActor_ver1::AVRActor_ver1():
 	// Input Action「IA_Look」を読み込む
 	LookAction = LoadObject<UInputAction>(nullptr, TEXT("/Game/Kanda/Input/IA_Look"));
 
+	// Input Action「IA_MoveAction」を読み込む
+	MoveStart = LoadObject<UInputAction>(nullptr, TEXT("/Game/Kanda/Input/IA_MoveAction"));
+
 	// テスト用
 	{
 		// 現在時刻の取得
@@ -80,7 +84,7 @@ AVRActor_ver1::AVRActor_ver1():
 void AVRActor_ver1::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
 	//Add Input Mapping Context
 	if (const APlayerController* PlayerController = Cast<APlayerController>(Controller))
 	{
@@ -96,6 +100,16 @@ void AVRActor_ver1::BeginPlay()
 			GEngine->XRSystem->SetTrackingOrigin(EHMDTrackingOrigin::Local);
 		}
 	}
+
+	// スプラインをレベル上から取得（一個だけ） また、プレイヤーの初期位置をセット
+	SplineActor = Cast<APlayerWayRoad>(UGameplayStatics::GetActorOfClass(GetWorld(), APlayerWayRoad::StaticClass()));
+	if (SplineActor) // ただのnullチェック
+	{
+		FTransform transformTemp;
+		transformTemp = SplineActor->GetSplineTransform(distance, 0.0f);
+		FVector newLocation = FVector(transformTemp.GetLocation());
+		SetActorLocation(newLocation);
+	}
 	IsInMagicZone = false;
 }
 
@@ -104,6 +118,16 @@ void AVRActor_ver1::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 	//VRInformation();
+
+	ArriveSplinePoint(StopPointNum);
+	// スプラインの上を移動していく処理
+	if (SplineActor && !isStop) // ただのnullチェック&今停止中かチェック
+	{
+		FTransform transformTemp;
+		transformTemp = SplineActor->GetSplineTransform(distance, MoveSpeedPoint * DeltaTime);
+		FVector newLocation = FVector(transformTemp.GetLocation());
+		SetActorLocation(newLocation);
+	}
 }
 
 // Called to bind functionality to input
@@ -123,6 +147,9 @@ void AVRActor_ver1::SetupPlayerInputComponent(UInputComponent* PlayerInputCompon
 
 		// LookとIA_LookのTriggeredをBindする
 		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &AVRActor_ver1::Look);
+
+		// MoveStartをバインドする
+		EnhancedInputComponent->BindAction(MoveStart, ETriggerEvent::Triggered, this, &AVRActor_ver1::PlayerMoveStart);
 	}
 }
 
@@ -134,7 +161,7 @@ void AVRActor_ver1::ControlPlayer(const FInputActionValue& Value)
 	FVector PreLocation = GetActorLocation();
 
 	FVector NewLocation = PreLocation + Sphere->GetComponentToWorld().TransformVectorNoScale(FVector(V.Y, V.X, 0.0f) * MoveSpeedPoint);
-	
+
 	SetActorLocation(NewLocation);
 }
 
@@ -146,10 +173,10 @@ void AVRActor_ver1::ChargeMagic(const FInputActionValue& Value)
 		{
 			UKismetSystemLibrary::PrintString(
 				this,
-				TEXT("5byoutattayo"),
+				TEXT("2byoutattayo"),
 				true,
 				true,
-				FColor(255, 255, 255, 0),
+				FColor::Cyan,
 				2.0f
 			);
 		}
@@ -165,7 +192,7 @@ void AVRActor_ver1::GoMagic(const FInputActionValue& Value)
 			TEXT("uttayo"),
 			true,
 			true,
-			FColor(255, 255, 255, 1),
+			FColor::Red,
 			2.0f
 		);
 		if (magicData == nullptr) { return; }
@@ -174,7 +201,7 @@ void AVRActor_ver1::GoMagic(const FInputActionValue& Value)
 			TEXT("HELLO"),
 			true,
 			true,
-			FColor(1,1,1,1),
+			FColor(1, 1, 1, 1),
 			2.0f
 		);
 		const int cnt = magicData->GetMagicCnt();
@@ -268,7 +295,7 @@ void AVRActor_ver1::OnSphereBeginOverlap(UPrimitiveComponent* OverlappedComp, AA
 }
 
 // 接触判定の処理、コライダー同士が離れたときに呼び出される
-void AVRActor_ver1::OnSphereEndOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex){
+void AVRActor_ver1::OnSphereEndOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex) {
 
 	IsInMagicZone = false;
 	if (AOnishi_MagicCircleParent* Pawn = Cast<AOnishi_MagicCircleParent>(OtherActor)) {
@@ -327,11 +354,32 @@ void AVRActor_ver1::VRInformation()
 		this->Sphere->SetRelativeLocationAndRotation(Position, OrientationAsQuat);
 	}
 }
-// スプラインのTransformを取得
-void AVRActor_ver1::GetSplineTransform(float& distance, float speed)
+
+// IA_MoveActionに登録されたボタンを押されると行う処理（現在はJキー）
+void AVRActor_ver1::PlayerMoveStart(const FInputActionValue& Value)
 {
-	double value = distance + speed;
-	double min = 0.0;
-	double max = 1.0;
-	UKismetMathLibrary::FWrap(value, min, max);
+	//ストップしている時のみ処理
+	if (isStop)
+	{
+		UKismetSystemLibrary::PrintString(this, TEXT("ugoke"), true, true, FColor::Blue, 2.0f, NAME_None);
+		isStop = false;
+		StopPointNum++;
+	}
+}
+
+// 指定したスプラインの点に到達すると行われる処理
+void AVRActor_ver1::ArriveSplinePoint(int point_)
+{
+	// ニアリーイコールを使うための変数たち
+	FVector SplinePoint = SplineActor->Spline->GetLocationAtSplinePoint(point_, ESplineCoordinateSpace::World);
+	FTransform ATransform = FTransform(FRotator(0, 0, 0), SplinePoint, FVector(1, 1, 1));
+	FVector nowLocation = GetActorLocation();
+	FTransform BTransform = FTransform(FRotator(0, 0, 0), nowLocation, FVector(1, 1, 1));
+
+	// ニアリーイコールを使ってキャラクターを止めるか動かすか判断
+	bool isNearPointCharacter = UKismetMathLibrary::NearlyEqual_TransformTransform(ATransform, BTransform, 5.0f, 0.0001f, 0.0001f);
+	if (isNearPointCharacter)
+	{
+		isStop = true;
+	}
 }
