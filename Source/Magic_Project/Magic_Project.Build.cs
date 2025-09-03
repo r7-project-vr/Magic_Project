c974759a69a5f90bd0a8c86b4ce0281504df72b0
satou <24CI0412@jec.ac.jp>
2025-09-03 19:53:25 +0900

    クロスヘアの追加、その方向に魔法が飛ぶように調整、パッケージ化して出た問題を一部解決

--- Source/Magic_Project/Magic_Project.Build.cs
+++ Source/Magic_Project/Magic_Project.Build.cs
@@ -8,7 +8,7 @@ public class Magic_Project : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG", "Niagara", "HeadMountedDisplay", "ASerialCom" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG", "Niagara", "HeadMountedDisplay", "ASerialCom", "XRBase" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
